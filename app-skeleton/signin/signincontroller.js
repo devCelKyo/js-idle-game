dbede28a993599e52a15ac2951fe6907329f27da
BOUDA Oussama <ouss937@gmail.com>
2021-12-09 23:33:10 +0100

    OBO: fonction pour post formulaire, variables du form

--- app-skeleton/signin/signincontroller.js
+++ app-skeleton/signin/signincontroller.js
@@ -1,6 +1,6 @@
-myApp.controller('SigninController', ["$scope", "$state","sampleFactory",
+myApp.controller('SigninController', ["$scope", "$state","sampleFactory", "$http",
 
-  function($scope, $state, sampleFactory) {
+  function($scope, $state, sampleFactory, $http) {
 
     console.log('this is the signincontroller, hi!');
 
@@ -8,7 +8,23 @@ myApp.controller('SigninController', ["$scope", "$state","sampleFactory",
 
     $scope.gotohome = function() {
       $state.go("home");
-    }
+    };
+
+    $scope.username = "";
+    $scope.password = "";
+
+    $scope.register = function() {
+      var donnee = {
+        username : $scope.username,
+        password : $scope.password
+      };
+      donnee = JSON.stringify(donnee);
+      console.log(donnee);
+      $http.post('http://localhost:20000', donnee)
+      .then(function(response) {
+          console.log(response.data);
+      });
+    };
 
   }
 ]);
